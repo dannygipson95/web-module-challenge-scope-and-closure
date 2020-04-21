3673b9c1a30923e83269f453a7b1ecc0b0486f94
Daniel Gipson <dannygipson95@gmail.com>
2020-04-21 18:45:24 -0400

    Completed the Challenges

--- index.js
+++ index.js
@@ -28,10 +28,15 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * Count is in the function in counter1 and outside in counter2
+ * 
  * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * The second one, because the variable is declared outside of the function.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
+ * If you don't want to use the count variable outside of the function counter 1 would be preferrable, otherwise counter two should be used.
 */
 
 // counter1 code
@@ -56,11 +61,12 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
+function inning(){
+  let random = Math.round(Math.random() * 2);
+  return random;
 
 }
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -76,12 +82,18 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function getInningScore(inning, numInnings){
+  let homeTeam = 0;
+  let awayTeam = 0;
+  for (let i = 0; i < numInnings; i++){
+    homeTeam = homeTeam + inning();
+    awayTeam = homeTeam + inning();
+  }
+  return { 'Home': homeTeam, 'Away': awayTeam};
 
-  /*Code Here*/
+};
 
-}
-
+console.log(getInningScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -104,8 +116,39 @@ and returns the score at each pont in the game, like so:
 
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(numInnings, getInningScore, inning) {
+  let scores = [];
+  for (let i = 0; i < numInnings; i++){
+    scores.push(getInningScore(inning, 1));
+  };
+
+  let finalHome = scores.reduce(function(accumulator, item){
+    return accumulator + item.Home;
+  },0);
+
+  let finalAway = scores.reduce(function(accumulator, item){
+    return accumulator + item.Away;
+  },0);
+
+  let inningNums = [];
+  function getInnings(numInnings) {
+  for(let i = 0; i < numInnings; i++){
+    if (i === 0){
+      inningNums.push(`1st inning: ${scores[i].Home} - ${scores[i].Away}`);
+    } else if (i === 1){
+      inningNums.push(`2nd inning: ${scores[i].Home} - ${scores[i].Away}`);
+    } else if (i === 2){
+      inningNums.push(`3rd inning: ${scores[i].Home} - ${scores[i].Away}`);
+    } else {
+      inningNums.push(`${i + 1}th inning: ${scores[i].Home} - ${scores[i].Away}`);
+    }
+  }
+  inningNums.push(`Final Score: ${finalHome} - ${finalAway}`)
 }
+getInnings(numInnings);
+  return inningNums;
+}
+
+console.log(scoreboard(9, getInningScore, inning));
 
 
